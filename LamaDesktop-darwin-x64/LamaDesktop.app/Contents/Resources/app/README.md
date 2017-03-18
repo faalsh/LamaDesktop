1df644e9b00c3de64336a7cf1fc0bc04f869814b
faalsh <fahad@Fahads-MacBook-Pro.local>
2017-03-18 14:44:19 +0300

    remove electron build

--- LamaDesktop-darwin-x64/LamaDesktop.app/Contents/Resources/app/README.md
+++
@@ -1,4 +0,0 @@
-[![Build Status](https://travis-ci.org/faalsh/Lama.svg?branch=master)](https://travis-ci.org/faalsh/Lama)
-
-# Lama
-
--- LamaDesktop-darwin-x64/LamaDesktop.app/Contents/Resources/app/src/common/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {applyMiddleware, combineReducers} from 'redux'
-import main from './main'
-
-export default combineReducers({
-	main
-})
